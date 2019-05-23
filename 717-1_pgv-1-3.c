25cda1554ac8b7891244ffca5e4f1f6d7a55ba34
Петриченко <717-1_pgv@36411.cloud.hosted-by-virtualdc.ru>
2019-05-24 00:06:57 +0700

    commit

---
+++ 717-1_pgv-1-3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int x;
+    int y;
+    int num3;
+    scanf( "%d", &x );
+    scanf( "%d", &y );
+    num3 = pow(x,y);
+    printf("%d", num3);
+    getchar();
+    return 0;
+}
